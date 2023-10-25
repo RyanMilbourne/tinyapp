abe9fdcc922db9c031629ed9b3aea6ffca6e688a
Ryan Milbourne <ryanmmilbourne@gmail.com>
2023-10-25 11:56:36 -0700

    new page urls/new rendering urls_new.ejs template with form button type POST

--- express_server.js
+++ express_server.js
@@ -4,11 +4,16 @@ const PORT = 8080;
 
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({ extended: true }));
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+function generateRandomString() {
+
+};
 
 app.get("/", (req, res) => {
   res.send("hello, user!");
@@ -28,10 +33,19 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVals);
 })
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 })
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
 
 app.listen(PORT, () => {
   console.log(`app listeing on port: ${PORT}`);
